632a0c314fbc799fb87ea8387fe3d521fa22aff9
Marija Petrovic <marijap@protonmail.com>
2023-01-22 23:52:53 +0100

    create initial project for unsss application

---
+++ unsss/src/main/java/com/example/unsss/UnsssApplication.java
@@ -0,0 +1,13 @@
+package com.example.unsss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsssApplication.class, args);
+	}
+
+}
---
+++ unsss/src/test/java/com/example/unsss/UnsssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unsss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
