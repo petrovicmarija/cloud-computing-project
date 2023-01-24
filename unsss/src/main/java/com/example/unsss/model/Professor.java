55ed5399284f9db578e5b11d3a447c371b27f60c
Marija Petrovic <marijap@protonmail.com>
2023-01-24 08:33:24 +0100

    change db driver

--- unsss/src/main/java/com/example/unsss/model/Professor.java
+++ unsss/src/main/java/com/example/unsss/model/Professor.java
@@ -1,15 +1,13 @@
 package com.example.unsss.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "professors")
 public class Professor {
 
     @Id
