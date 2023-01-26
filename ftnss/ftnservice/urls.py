e631cdacc1b9f8f3dbd4bc99f7dc446592801bf4
Marija Petrovic <marijap@protonmail.com>
2023-01-26 01:14:22 +0100

    change path

--- ftnss/ftnservice/urls.py
+++ ftnss/ftnservice/urls.py
@@ -5,8 +5,6 @@ urlpatterns = [
     path('ftnservice', views.index, name='home'),
     path('ftnservice/add-student', views.add_student),
     path('ftnservice/add-professor', views.add_professor),
-    path('ftnservice/students', views.save_new_student),
-    path('ftnservice/professors', views.save_new_professor),
     path('ftnservice/students', views.get_students, name='list-students'),
     path('ftnservice/professors', views.get_professors, name='list-professors'),
 ]
